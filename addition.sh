34d486250cb802fb4ed2606cfe5a5101e825a27a
pavanwa <pavanmuppalla1@gmail.com>
2023-12-12 10:59:15 +0530

    add file

--- addition.sh
+++ addition.sh
@@ -3,4 +3,8 @@
 num1=10
 num2=20
 
-echo  $(( num1 + num2 ))
+echo  $(expr $num1 + $num2 )
+echo $(expr $num1 - $num2 )
+echo $(expr $num1 \* $num2 )
+echo $(expr $num1 / $num2 )
+echo $(expr $num1 % $num2 )
